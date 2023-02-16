9a08acd2702c4214ec021419fff2c7fccd770ce2
lealul19 <94169536+lealul19@users.noreply.github.com>
2023-02-16 11:04:13 +0100

    Add files via upload

---
+++ MED2/javascript/meinScript.js
@@ -0,0 +1,7 @@
+//Der prompt-Befehl erzeugt ein Eingabefenster.
+//prompt("Wie alt bist du?");
+
+
+// Text steht immer unter Anführungszeichen.
+//Um Text und Variablen zu kombinieren, wird das Pluszeichen verwendet.
+alert(" du bist " + (prompt(" Wie alt bist du?")) + " Jahre alt");
